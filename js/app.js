0845150c56568795990a1b408cdd15f04995dbe6
Pedro Arvellos <pedroarvellos@hotmail.com>
2018-04-23 09:27:16 -0300

    Rendering and dynamically upgrading images and including new functionalities, like characters' moviments, bugs' moviments and loops and moviment validation

--- js/app.js
+++ js/app.js
@@ -1,46 +1,95 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+let allEnemies = [];
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+var Enemy = function(xValue, yValue) {
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
+
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  this.sprite = "images/enemy-bug.png";
+  this.x = xValue;
+  this.y = yValue;
+  this.arbitratyNumber = 150;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
+  if (this.x >= ctx.canvas.clientWidth) {
+    getClean.call(this);
+  }
+
+  this.x += this.arbitratyNumber * dt;
 };
+
+function getClean() {
+  this.x = -90;
+  //so I get a random value between the max and min value.
+  this.arbitratyNumber = Math.random() * (400 - 100) + 100;
+}
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function(xValue, yValue) {
+  this.sprite = "images/char-boy.png";
+  this.x = xValue;
+  this.y = yValue;
+};
+
+Player.prototype.update = function(dt) {};
+
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(key) {
+  if (key == "up" && player.y > 0) {
+    player.y -= 80;
+  }
+  if (key == "right" && player.x < 400) {
+    player.x += 100;
+  }
+  if (key == "down" && player.y < 380) {
+    player.y += 80;
+  }
+  if (key == "left" && player.x > 0) {
+    player.x -= 100;
+  }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+enemy1 = new Enemy(0, 60);
+enemy2 = new Enemy(0, 140);
+enemy3 = new Enemy(0, 220);
+allEnemies.push(enemy1);
+allEnemies.push(enemy2);
+allEnemies.push(enemy3);
 
+player = new Player(200, 380);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+document.addEventListener("keyup", function(e) {
+  var allowedKeys = {
+    37: "left",
+    38: "up",
+    39: "right",
+    40: "down"
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
